8e1119d919d6c66323e650037767b99d96e2ad8c
Brandon Ladd <ladd.b@aol.com>
2023-05-17 14:48:19 -0700

    includes tables

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-
-#SET PAGE CONFIGURATION:
-st.set_page_config(page_title= "Sales Dashboard",
+# SET PAGE CONFIGURATION:
+st.set_page_config(page_title="Sales Dashboard",
                    page_icon=":balloon:",
                    layout="wide")
 
-#HIDE THE HAMBURGER ICON IN TOP RIGHT:
+# HIDE THE HAMBURGER ICON IN TOP RIGHT:
 st.markdown(
     """
     <style>
@@ -18,78 +17,81 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-#ADD THE TITLE:
+# ADD THE TITLE:
 st.title("Superstore Sales Dashboard")
 st.markdown("---")
 
 
-#CACHE THE EXCEL FILE SO YOU DON'T HAVE TO KEEP RELOADING IT
+# CACHE THE EXCEL FILE SO YOU DON'T HAVE TO KEEP RELOADING IT:
 @st.cache_data
 def excel_sheet_cache():
-    df = pd.read_excel(
+    dfx = pd.read_excel(
         io="C:\\Users\\Brandon\\Downloads\\super.xlsx",
         engine='openpyxl',
         sheet_name='Orders',
         usecols='A:U',
         nrows=9996,
     )
-    return df
+    return dfx
 
+
+# NAME THE DATAFRAME AFTER CACHING IT:
 df = excel_sheet_cache()
 
-#ASSIGN STATES TO THE PROPER REGIONS:
+# ASSIGN STATES TO THE PROPER REGIONS:
 
 regions = {
-    'East' : ['Pennsylvania', 'Delaware', 'New York', 'Ohio', 'Connecticut', 'New Jersey', 'Massachusetts', 'Rhode Island', 'New Hampshire', 'Maryland', 'District of Columbia', 'Vermont', 'Maine', 'West Virginia'],
-    'West' : ['California', 'Washington', 'Utah', 'Arizona', 'Oregon', 'Colorado', 'New Mexico', 'Nevada', 'Montana', 'Idaho', 'Wyoming'],
-    'Central' : ['Texas', 'Wisconsin', 'Nebraska', 'Illinois', 'Minnesota', 'Michigan', 'Indiana', 'Iowa', 'Missouri', 'Oklahoma', 'Kansas', 'South Dakota', 'North Dakota' ],
-    'South' : ['Kentucky', 'Florida', 'North Carolina', 'Virginia', 'Tennessee', 'Alabama', 'South Carolina', 'Louisiana', 'Georgia', 'Mississippi', 'Arkansas']
+    'East': ['Pennsylvania', 'Delaware', 'New York', 'Ohio', 'Connecticut', 'New Jersey', 'Massachusetts',
+             'Rhode Island', 'New Hampshire', 'Maryland', 'District of Columbia', 'Vermont', 'Maine', 'West Virginia'],
+    'West': ['California', 'Washington', 'Utah', 'Arizona', 'Oregon', 'Colorado', 'New Mexico', 'Nevada', 'Montana',
+             'Idaho', 'Wyoming'],
+    'Central': ['Texas', 'Wisconsin', 'Nebraska', 'Illinois', 'Minnesota', 'Michigan', 'Indiana', 'Iowa', 'Missouri',
+                'Oklahoma', 'Kansas', 'South Dakota', 'North Dakota'],
+    'South': ['Kentucky', 'Florida', 'North Carolina', 'Virginia', 'Tennessee', 'Alabama', 'South Carolina',
+              'Louisiana', 'Georgia', 'Mississippi', 'Arkansas']
 }
 
-#SIDEBAR
+# SIDEBAR
 st.sidebar.header("Filter:")
 
-
-
+# SELECT REGION:
 region = st.sidebar.multiselect(
     "Select Region:",
     options=list(regions.keys()),
     default=list(regions.keys())
 
- )
+)
 
-#DYNAMICALLY UPDATE STATES BASED ON SELECTED REGION
-
+# DYNAMICALLY UPDATE STATES BASED ON SELECTED REGION:
 states = [state for r in region for state in regions.get(r, [])]
 
+# SELECT STATE:
 state = st.sidebar.multiselect(
     "Select State:",
     options=states,
     default=states
 
- )
+)
 
-
+# SELECT CATEGORY:
 category = st.sidebar.multiselect(
     "Select Category:",
     options=df["Category"].unique(),
     default=df["Category"].unique()
 
- )
+)
 
-
+# UPDATE THE DATAFRAME TO INCLUDE ONLY THE SELECTED OPTIONS:
 selected = df.query(
     "Region == @region & State in @state & Category == @category"
 )
 
-
-
+# IF NOTHING IS SELECTED, THROW A WARNING MESSAGE:
 if selected.empty:
     st.warning("Please select a valid option.")
 
 
-#Key Performance Indicators (KPI's)
+# CALCULATE KEY PERFORMANCE INDICATORS (KPI'S):
 else:
     total_sales = int(selected["Sales"].sum())
     total_profit = int(selected["Profit"].sum())
@@ -101,6 +103,7 @@ else:
     least_discounts = min(selected["Discount"])
     expenses = int(total_sales - total_profit)
 
+    # CREATE 6 DIFFERENT COLUMNS TO DISPLAY KPI'S:
     col1, col2, col3, col4, col5, col6 = st.columns(6)
 
     with col1:
@@ -124,16 +127,14 @@ else:
 
     st.markdown("---")
 
-
-
-    #Use Pandas groupby() to organize data by Category and total sales
+    # USE PANDAS GROUPBY() TO ORGANIZE DATA BY CATEGORY AND TOTAL SALES:
 
     category_sales = (
         selected.groupby(by=["Category"]).sum()[["Sales"]]
 
     )
 
-    #SALES BY PRODUCT LINE [BAR CHART]
+    # SALES BY PRODUCT LINE [BAR CHART]
 
     bar_chart_figure = px.bar(
         category_sales,
@@ -143,9 +144,9 @@ else:
 
     )
 
-    #UPDATE BAR CHART SO THAT THE BACKGROUND IS TRANSPARENT AND GIVE IT A TITLE
+    # UPDATE BAR CHART SO THAT THE BACKGROUND IS TRANSPARENT AND GIVE IT A TITLE
     bar_chart_figure.update_layout(
-        plot_bgcolor = "rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
         title={
             'text': '<b>Sales by Category</b>',
             'x': 0.5,
@@ -155,11 +156,11 @@ else:
         }
     )
 
-
-
+    # DEFINE NAMES AND VALUES FOR PIE CHART:
     names = selected["Region"]
     values = selected["Sales"]
 
+    # SALES BY REGION [PIE CHART]:
     pie_chart_figure = px.pie(
         category_sales,
         values=values,
@@ -167,58 +168,37 @@ else:
         template="plotly_dark"
     )
 
-    #UPDATE THE BACKGROUND OF THE CHART TO TRANSPARENT AND COLOR OF THE LEGEND SO THAT IS IS VISIBLE IN LIGHT/DARK MODE:
+    # UPDATE THE BACKGROUND OF THE CHART TO TRANSPARENT AND GIVE IT A TITLE:
 
     pie_chart_figure.update_layout(
-        plot_bgcolor = "rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
         legend=dict(
             bgcolor="Blue"
         ),
         title={
             'text': '<b>Sales by Region</b>',
-            'x':0.5,
+            'x': 0.5,
             'xanchor': 'center',
             'yanchor': 'top',
-            'font': {'size':24}
+            'font': {'size': 24}
         }
     )
-
+    # CREATE 2 COLUMNS TO FORMAT THE BAR AND PIE CHART ON THE SCREEN:
     maincol1, maincol2 = st.columns(2)
 
     maincol1.plotly_chart(bar_chart_figure, use_container_width=True)
     maincol2.plotly_chart(pie_chart_figure, use_container_width=True)
 
-    #HIGHEST PROFITING STATES TABLE:
+    # HIGHEST PROFITING STATES TABLE:
 
     highest_profit_state = selected.groupby("State").sum().sort_values("Profit", ascending=False).head(10).reset_index()
     highest_profit_state.index = highest_profit_state.index + 1
     st.subheader("Highest Profiting States")
     st.table(highest_profit_state[["State", "Profit"]])
 
-    #TOTAL SALES BY REGION TABLE:
+    # TOTAL SALES BY REGION TABLE:
 
     total_sales_by_region = selected.groupby("Region").sum().sort_values("Sales", ascending=False).reset_index()
     total_sales_by_region.index = total_sales_by_region.index + 1
     st.subheader("Total Sales by Region")
     st.table(total_sales_by_region[["Region", "Sales"]])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
